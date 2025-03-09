c4eafa8912e1ae0e386c5946ca409870abff1849
Skymore <122345615@qq.com>
2025-03-09 04:39:58 -0500

    refactor: remove React import from ZustandTest component

--- src/components/ZustandTest.tsx
+++ src/components/ZustandTest.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTreatingStore } from '../stores/treatingStore';
 import { v4 as uuidv4 } from 'uuid';
 
